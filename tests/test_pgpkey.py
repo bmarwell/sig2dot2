d532ffc35d932b986eb18482ad6249925bd76ac4
Paal Braathen <paalbra@gmail.com>
2018-12-28 12:14:27 +0100

    Add initial tests of OpenPGPKey and OpenPGPSig

---
+++ tests/test_pgpkey.py
@@ -0,0 +1,41 @@
+import os.path
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import sig2dot.gpg.OpenPGPKey
+
+def test_blank_key():
+    result = sig2dot.gpg.OpenPGPKey.OpenPGPKey()
+
+    assert result.id == ""
+    assert result.creationdate == 0
+    assert result.expireydate == 0
+    assert result.name == ""
+    assert result.comment == ""
+    assert result.email == ""
+    assert not result.sigs
+    assert not result.signed
+
+def test_key_properties():
+    _id = "ABCDEF0123456789"
+    creation_date = "1514764800"
+    expiry_date = "1577836800"
+    name = "Bob Example"
+    comment = "Example Inc"
+    email = "bob@example.com"
+
+    result = sig2dot.gpg.OpenPGPKey.OpenPGPKey()
+    result.id = _id
+    result.creationdate = creation_date
+    result.expireydate = expiry_date
+    result.name = name
+    result.comment = comment
+    result.email = email
+
+    assert result.id == _id
+    assert result.creationdate == creation_date
+    assert result.expireydate == expiry_date
+    assert result.name == name
+    assert result.comment == comment
+    assert result.email == email
---
+++ tests/test_pgpsig.py
@@ -0,0 +1,27 @@
+import os.path
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import sig2dot.gpg.OpenPGPSig
+
+def test_blank_sig():
+    result = sig2dot.gpg.OpenPGPSig.OpenPGPSig()
+
+    assert result.id == ""
+    assert result.signdate == -1
+    assert result.expirydate == -1
+
+def test_sig_properties():
+    _id = "ABCDEF0123456789"
+    sign_date = "1514764800"
+    expiry_date = "1577836800"
+
+    result = sig2dot.gpg.OpenPGPSig.OpenPGPSig()
+    result.id = _id
+    result.signdate = sign_date
+    result.expirydate = expiry_date
+
+    assert result.id == _id
+    assert result.signdate == sign_date
+    assert result.expirydate == expiry_date
